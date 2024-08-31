8f18a19a389889c9339d19b27ba0b16cd83e4137
nissim-sheode <nissimsheode@gmail.com>
2024-08-31 14:11:25 +0530

    Checking in react concepts

---
+++ App.js
@@ -0,0 +1,21 @@
+// const  heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "Hello World from React!");
+// console.log(heading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const  parent = React.createElement("div", {id: "parent"},[
+    React.createElement("div", {id: "child1"}, [
+        React.createElement("h1", {id: "heading_1"}, "Heading 1"), 
+        React.createElement("h2", {id: "heading_2"}, "Heading 2"),
+    ]),
+
+    React.createElement("div", {id: "child2"}, [
+        React.createElement("h1", {id: "heading_1"}, "Heading 1"), 
+        React.createElement("h2", {id: "heading_2"}, "Heading 2"),
+    ]),
+]);
+
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
