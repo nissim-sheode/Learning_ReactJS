c49d65c592600fd0c90f1dc7489e6b66cd125775
nissim-sheode <nissimsheode@gmail.com>
2024-08-31 14:16:42 +0530

    Change in App.js file

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ const  parent = React.createElement("div", {id: "parent"},[
     React.createElement("div", {id: "child2"}, [
         React.createElement("h1", {id: "heading_1"}, "Heading 1"), 
         React.createElement("h2", {id: "heading_2"}, "Heading 2"),
+        React.createElement("h3", {id: "heading_3"}, "Heading 3")
     ]),
 ]);
 
